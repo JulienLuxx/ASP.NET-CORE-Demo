fd0e11d681bea69b65f063bfa956364083eebe1c
JulienLuxx <JulienLux@outlook.com>
2019-08-02 16:22:06 +0800

    ImproveLogMiddleware

--- Web/Test.Web/Middleware/ExceptionMiddleware.cs
+++ Web/Test.Web/Middleware/ExceptionMiddleware.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http.Internal;
+using System.Text;
 
 namespace Test.Web.Middleware
 {
@@ -30,10 +32,12 @@ namespace Test.Web.Middleware
 
         public async Task Invoke(HttpContext context)
         {
+            var guid = Guid.NewGuid();
             try
             {
                 await _requestDelegate.Invoke(context);
-                var features = context.Features;
+                //var features = context.Features;
+                await HandleLog(guid, context);
             }
             catch (Exception e)
             {
@@ -41,15 +45,44 @@ namespace Test.Web.Middleware
             }
         }
 
+        private async Task HandleLog(Guid guid, HttpContext context)
+        {
+            var info = string.Empty;
+            var param = new Dictionary<string, string>();
+            if (context.Request.ContentLength.HasValue)
+            {
+                if (context.Request.HasFormContentType)
+                {
+                    var form = context.Request.Form;
+                    param = form.ToDictionary(x => x.Key, y => y.Value.FirstOrDefault());
+                }
+                else if (context.Request.ContentLength > 0)
+                {
+                    var bytes = new byte[context.Request.Body.Length];
+                    context.Request.EnableRewind();
+                    context.Request.Body.Seek(0, 0);
+                    await context.Request.Body.ReadAsync(bytes, 0, bytes.Length);
+                    var jsonParam = Encoding.UTF8.GetString(bytes);
+                    param = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonParam);
+                }
+            }
+            if (context.Request.QueryString.HasValue)
+            {
+                param = context.Request.Query.ToDictionary(x => x.Key, y => y.Value.FirstOrDefault());
+            }
+            info = JsonConvert.SerializeObject(new { Id = guid, ClientAddress = context.Connection.RemoteIpAddress.ToString() + ":" + context.Connection.RemotePort.ToString(), RequestUrl = context.Request.Host + context.Request.Path, Param = param });
+            _logger.LogInformation(info);
+        }
+
         private async Task HandleExcetion(HttpContext context, Exception exception)
         {
             context.Response.StatusCode = 500;
-            context.Response.ContentType = "text/json;charset=utf-8";
-            var error = string.Empty;
+            context.Response.ContentType = "text/json;charset=utf-8;";
+            string error = string.Empty;
 
             void ReadException(Exception ex)
             {
-                error += string.Format("{0}  |  {1}  |  {2}", ex.Message, ex.StackTrace, ex.InnerException);
+                error += string.Format("{0} | {1} | {2}", ex.Message, ex.StackTrace, ex.InnerException);
                 if (null != ex.InnerException)
                 {
                     ReadException(ex.InnerException);
@@ -57,16 +90,29 @@ namespace Test.Web.Middleware
             }
 
             ReadException(exception);
+
             if (_environment.IsDevelopment())
             {
-                var json = new { message = exception.Message, detail = error };
-                error = JsonConvert.SerializeObject(json);
+                error = JsonConvert.SerializeObject(new
+                {
+                    Id = guid,
+                    message = exception.Message,
+                    detail = error
+                });
                 _logger.LogError(error);
             }
             else
             {
-                error = "Sorry,Error!";
+                error = JsonConvert.SerializeObject(new
+                {
+                    Id = guid,
+                    message = exception.Message,
+                    detail = error
+                });
+                _logger.LogError(error);
+                error = "抱歉，出错了";
             }
+
             await context.Response.WriteAsync(error);
         }
     }
